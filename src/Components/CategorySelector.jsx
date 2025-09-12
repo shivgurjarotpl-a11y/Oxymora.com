931820e7d4ac93462558042952d62b4a9658ed76
shivgurjar <tumhara-github-email>
2025-09-12 17:34:59 +0530

    button ui change

--- src/Components/CategorySelector.jsx
+++ src/Components/CategorySelector.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "../css/CategorySelector.css"; // external css import
-import { Globe, Smartphone, Monitor } from "lucide-react";
+import { Globe, Smartphone, Monitor, LayoutDashboard } from "lucide-react";
 
 const CategorySelector = ({ active, setActive }) => {
   const categories = [
     { id: "all", label: "All", icon: <Globe size={28} /> },
-    { id: "application", label: "Application", icon: <Smartphone size={28} /> },
-    { id: "website", label: "Website", icon: <Monitor size={28} /> },
+    { id: "application", label: "Iot Application", icon: <Smartphone size={28} /> },
+    { id: "website", label: "Iot Dashboard", icon: <Monitor size={28} /> },
+    { id: "Projects", label: "Projects", icon: <LayoutDashboard size={28} /> },
   ];
 
   return (
